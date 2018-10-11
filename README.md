d9cf892180584599d6a543e27e3a1a6746faa0a7
Youssef Eldakar <youssefeldakar@gmail.com>
2018-10-11 17:09:52 +0200

    Recursively color neighboring pixels in a solid region

---
+++ README.md
@@ -0,0 +1,8 @@
+# Recreational Programming: Floodfill
+
+This is a recreational programming exercise implementing an image flood
+fill algorithm.
+
+Read about flood fill on Wikipedia:
+
+https://en.wikipedia.org/wiki/Flood_fill
---
+++ highlighter.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+from PIL import Image
+import sys
+import random
+
+sys.setrecursionlimit(1000000)
+
+blu = (0, 0, 255, 255)
+
+img = Image.open(sys.argv[1])
+w, h = img.width, img.height
+bmap = img.load()
+
+fuzz = (random.choice(range(0, w - 1)), random.choice(range(0, h - 1)))
+c = bmap[fuzz]
+
+def N(xy):
+    x, y = xy
+    if y > 0:
+        return (x, y - 1)
+
+def S(xy):
+    x, y = xy
+    if y < h - 1:
+        return (x, y + 1)
+
+def E(xy):
+    x, y = xy
+    if x < w - 1:
+        return (x + 1, y)
+
+def W(xy):
+    x, y = xy
+    if x > 0:
+        return (x - 1, y)
+
+def floodfill(xy):
+    if bmap[xy] != c:
+        return
+
+    bmap[xy] = blu
+
+    for ab in (N(xy), S(xy), E(xy), W(xy)):
+        if ab != None:
+            floodfill(ab)
+    return
+
+floodfill(fuzz)
+img.save(sys.argv[2])
+
+# vim: ts=4: sts=4: sw=4:
